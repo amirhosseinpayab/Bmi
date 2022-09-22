4970125e46074a96d230d86496a5342e3721b15b
Amirhossein Payab <89769597+amirhosseinpayab@users.noreply.github.com>
2022-09-22 14:40:25 +0330

    Update Bmi.py

--- Bmi.py
+++ Bmi.py
@@ -3,7 +3,7 @@ weight = float(input('Enter your weight(kg) : '))
 height = float(input('Enter your height(cm) : '))
 # Main fuction to calculate bmi 
 def bmi(weight , height):
-    bmi = weight / (height/100)**2
+    bmi = weight // (height/100)**2
     if bmi < 18.5:
         return '\nUnder weight'
     elif 18.5 <= bmi <= 24.9:
@@ -18,14 +18,14 @@ def bmi(weight , height):
         return '\nExtreme obesity'
 # Additional function to calculate how much lose or gain weight
 def normal_weight(weight , height):
-    bmi = weight / (height/100)**2
+    bmi = weight // (height//100)**2
     if bmi >= 25 :
-        normal = ((height/100)**2) * 24.9
+        normal = ((height//100)**2) * 24.9
         over_weight = weight - normal
         print('Your normal weight is %.0f kg'%normal)
         print('You have to lose %.0f kg\n'%over_weight)
     elif bmi < 18.5:
-        normal = ((height/100)**2) * 18.5
+        normal = ((height//100)**2) * 18.5
         under_weight = normal - weight
         print('Your normal weight is %.0f kg'%normal)
         print('You have to gain %.0f kg\n'%under_weight)
